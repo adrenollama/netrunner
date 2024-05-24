bd8fe76f93ae32a532d29570556b1012d04b15e9
adrenollama <155016039+adrenollama@users.noreply.github.com>
2024-05-23 23:56:16 -0300

    Update build.sh
    Updated buiild.sh, to add some default packages to the image.

--- build.sh
+++ build.sh
@@ -13,11 +13,21 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install distrobox
+rpm-ostree install libvirt
+rpm-ostree install libvirt-client
+rpm-ostree install libvirt-nss
+rpm-ostree install virt-manager
+rpm-ostree install fastfetch
+rpm-ostree install btop
+rpm-ostree install kvantum
+rpm-ostree install mangohud
+rpm-ostree install gamescope
+rpm-ostree install steam
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
