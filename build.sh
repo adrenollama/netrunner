a7dbcc03a1fcd10933a43328aa2160cb0db68046
adrenollama <155016039+adrenollama@users.noreply.github.com>
2024-05-29 17:06:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
+###  Custom repositories
+
+# Add third parties or custom repositories for additional software.
+wget https://download.opensuse.org/repositories/home:paul4us/Fedora_40/home:paul4us.repo -P /etc/yum.repos.d/
 
 ### Install packages
 
@@ -23,11 +27,18 @@ rpm-ostree install btop
 rpm-ostree install kvantum
 rpm-ostree install mangohud
 rpm-ostree install gamescope
-rpm-ostree install steam
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
+rpm-ostree install steam
+
+# this would install a package from a custom repository
+rpm-ostree install klassy
+
+# this would install a package from a RPM downloaded from the web
+rpm-ostree install https://github.com/ilya-zlobintsev/LACT/releases/download/v0.5.4/lact-0.5.4-0.x86_64.fedora-40.rpm
 
 #### Example for enabling a System Unit File
 
 #systemctl enable podman.socket
+systemctl enable lactd
